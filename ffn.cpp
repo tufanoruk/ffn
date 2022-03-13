4533942b6d6c9b6b91b3c32b26c532019a879c9c
Tufan Oruk <tufan.oruk@gmail.com>
2022-03-13 22:20:35 +0300

    score added.

--- ffn.cpp
+++ ffn.cpp
@@ -13,7 +13,7 @@
 // 7 is the middle number
 // 3 is furthest from 7
 //
-// This mimics a part of GIA test fır number speed and accuracy
+// This mimics a part of GIA test for number speed and accuracy
 //
 
 
@@ -30,7 +30,7 @@
 
 using namespace std;
 
-class FFTGame
+class FFNGame
 {
     class GameNumberGenerator
     {
@@ -57,7 +57,7 @@ class FFTGame
                 std::sort(q.begin(), q.end());
                 numbers_valid = (q[0] != q[1]) && (q[0] != q[2]) && (q[1] != q[2]) && ((q[1] - q[0]) != (q[2] - q[1])); 
             } while (!numbers_valid);
-            // std::random_shuffle does not exist in MacOs C++17!
+            // std::random_shuffle does not exist in MacOS C++17!
             //std::random_shuffle(std::begin(q), std::end(q), dist_);
             shuffle(q);
             return q; // return the copy of the int[3] array
@@ -81,25 +81,116 @@ class FFTGame
         Distribution dist_;  // distriburion of the generated numbers
     };
     
+
+    class Score {
+    public:
+        Score(){};
+
+        ~Score() {
+            int i = 0;
+            vector<string> q_and_a; 
+            for  (auto q : questions_) {
+                string s;
+                for (auto r : q) {
+                    s += std::to_string(r) + " ";  
+                }
+                s += " -> ";
+                s += std::to_string(answers_[i++]);
+                s += "\n";
+
+                q_and_a.push_back (s);
+            }
+            for (auto s : q_and_a) {
+                cout << s;
+            }
+
+        }
+        
+        void startChronometer() {
+            start_ = chrono::high_resolution_clock::now();
+        } 
+
+        void endChronometer() {
+            end_ = chrono::high_resolution_clock::now();
+        } 
+
+        void addQuestion (GameNumberGenerator::Question& q) {
+            questions_.push_back(q);
+        }
+
+        void addGivenAnswer (int a){
+            cout << "answer " << a << " added" << endl;
+            answers_.push_back(a);
+        }
+        
+        int getNumberOfQuestions(){
+            return questions_.size();
+        }
+
+        int getNumberOfCorrectAnswers(){            
+            // if an answer is "0" then it is either timed out or unanswered
+            int i = 0;
+            int numCorrectAnswers = 0;
+            for  (auto q : questions_) {
+                if (findAnswer(q) == answers_[i++]) {
+                    numCorrectAnswers++;
+                }
+            }
+            return numCorrectAnswers; 
+        }
+
+        // return elapsed time in milliseconds
+        int getEllapsedGameTime() {
+            auto duration = chrono::duration_cast<chrono::milliseconds> (end_ - start_);
+            return duration.count();
+        }
+
+        float getScore() {
+            return (getNumberOfQuestions() - (getNumberOfQuestions() - getNumberOfCorrectAnswers())) / (getEllapsedGameTime() / 1000);
+        }
+        
+    private:
+        // work on the copy of the question (pass by value)
+        int findAnswer (GameNumberGenerator::Question q) {
+            std::sort(q.begin(), q.end());
+            return ( (q[1] - q[0]) > (q[2] - q[1]) ? q[0] : q[2] );
+         }
+
+        chrono::time_point<chrono::steady_clock> start_;
+        chrono::time_point<chrono::steady_clock> end_;
+
+        vector<GameNumberGenerator::Question> questions_; 
+        vector<int> answers_; 
+    };
+
+
     enum {
         ANSWER_TIMEOUT = 4,
         BUF_LEN = 512
     };
 
 public:
-    FFTGame(int highestNum, boost::asio::io_context& ic) : ioContext_ (ic),
+    FFNGame(int highestNum, boost::asio::io_context& ic) : ioContext_ (ic),
                                                            numGen_(highestNum), 
                                                            input_(ic, ::dup(STDIN_FILENO)),
                                                            output_(ic, ::dup(STDOUT_FILENO)),
                                                            stream_buffer_(BUF_LEN),
-                                                           timer_(ic)
+                                                           timer_(ic),
+                                                           score_()
+
     {
         std::string msg{"Starting the game. Good luck!\n\n"};
         boost::asio::async_write(output_,
                                  boost::asio::buffer(msg.c_str(), msg.length()),
-                                 boost::bind(&FFTGame::askQuestion, this,
+                                 boost::bind(&FFNGame::askQuestion, this,
                                              boost::asio::placeholders::error));
+        score_.startChronometer();
     }
+
+    int numQuestionsAsked() {return score_.getNumberOfQuestions();}
+    int numCorrectAnswers() {return score_.getNumberOfCorrectAnswers();}
+    int gameDuration() {return score_.getEllapsedGameTime();}
+    float getScore() {return score_.getScore();}
 
 private:
     void timeOut(const boost::system::error_code &ec)
@@ -108,9 +199,11 @@ private:
         {
             std::string msg{"\nYou didn't give your anwer in time. Asking a new one!\n\n"};
 
+            score_.addGivenAnswer (0); // add no answer! 
+
             boost::asio::async_write(output_,
                                      boost::asio::buffer(msg.c_str(), msg.length()),
-                                     boost::bind(&FFTGame::askQuestion, this,
+                                     boost::bind(&FFNGame::askQuestion, this,
                                                  boost::asio::placeholders::error));
             timer_.cancel();
         }
@@ -126,14 +219,16 @@ private:
             cout << "\nAnswer (q to quit): "; 
             cout.flush();
 
+            score_.addQuestion (q);
+
             // get answer
             boost::asio::async_read_until(input_, stream_buffer_, '\n',
-                                          boost::bind(&FFTGame::getAnswer, this,
+                                          boost::bind(&FFNGame::getAnswer, this,
                                                       boost::asio::placeholders::error,
                                                       boost::asio::placeholders::bytes_transferred));
             int timeout = ANSWER_TIMEOUT;
             timer_.expires_from_now(boost::posix_time::seconds(timeout));
-            timer_.async_wait(boost::bind(&FFTGame::timeOut, this,
+            timer_.async_wait(boost::bind(&FFNGame::timeOut, this,
                                           boost::asio::placeholders::error));
         }
     }
@@ -144,16 +239,17 @@ private:
         {
             // get answer and ask question
             buffer_.fill('\0');
-            // read data from stream_buffer into buffer
+            // read data from stream_buffer upto \n into buffer  
             stream_buffer_.sgetn(buffer_.data(), len - 1);
-            stream_buffer_.consume(1); // Remove newline from input, otherwise it will be read!
+            stream_buffer_.consume(1); // Remove newline from input, otherwise it will be read again!
 
             if (buffer_[0] == 'q' || buffer_[0] == 'Q') {
                 ioContext_.stop();
+                score_.endChronometer();
                 return;
             }
 
-            //cout << "Your answer is " << "'" << buffer_.data() << "'" << endl;
+            score_.addGivenAnswer (std::atoi(buffer_.data()));
 
             static char prefix[] = {"Your answer is '"};
             static char suffix[] = {"'\n"};
@@ -162,7 +258,7 @@ private:
                                                                    boost::asio::buffer(suffix)}};
 
             boost::asio::async_write(output_, buffers,
-                                     boost::bind(&FFTGame::askQuestion, this,
+                                     boost::bind(&FFNGame::askQuestion, this,
                                                  boost::asio::placeholders::error));
         }
     }
@@ -175,6 +271,8 @@ private:
     boost::asio::posix::stream_descriptor output_;
     boost::asio::streambuf stream_buffer_;
     boost::asio::deadline_timer timer_;
+
+    Score score_;
 };
 
 // make sure the user has entered integer input and return it
@@ -222,9 +320,16 @@ int main(int, char**)
     }
 
     boost::asio::io_context ioc;
-    FFTGame fft(max, std::ref(ioc));
+    FFNGame ffn(max, std::ref(ioc));
 
-    ioc.run();    
+    ioc.run(); 
+
+    cout << "You quit the game____ " << endl;
+    cout << "Questions asked.....: " << ffn.numQuestionsAsked() << endl;
+    cout << "Correct answers.....: " << ffn.numCorrectAnswers() << endl;
+    cout << "Total game duration.: " << ffn.gameDuration() << " milliseconds" << endl;
+
+    cout << "Your score is " << ffn.getScore() << endl;
 
     return 0;
 }
